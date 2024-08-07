71ec6ec61f68797679e5a01cbb5ebee93e1f9972
Heather Haymond <hheatherjean11@gmail.com>
2024-08-07 00:28:49 -0500

    fixed mapping error

--- src/components/Market/Market.jsx
+++ src/components/Market/Market.jsx
@@ -4,11 +4,19 @@ import LogOutButton from "../LogOutButton/LogOutButton";
 import BuyButton from "../BuyButton/BuyButton";
 import UserInventoryPage from "../Inventory/UserInventoryPage";
 import ToggleButton from "../Inventory/ToggleButton";
-import { Container, Grid, Card, CardContent, CardActions, Typography, Button, TextField} from '@mui/material';
-import useCurrentPrices from '../../hooks/useCurrentPrices';
+import {
+  Container,
+  Grid,
+  Card,
+  CardContent,
+  CardActions,
+  Typography,
+  Button,
+  TextField,
+} from "@mui/material";
+import useCurrentPrices from "../../hooks/useCurrentPrices";
 
 // import UserChart from '../Charts/UserChart';
-
 
 const capitalizeFirstLetter = (string) => {
   return string.charAt(0).toUpperCase() + string.slice(1);
@@ -21,13 +29,15 @@ const replaceUnderscoreWithSpace = (string) => {
 const Market = () => {
   const dispatch = useDispatch();
   const user = useSelector((state) => state.user);
-  const fruits = useSelector((state) => state.fruit);
+  const fruits = useSelector((state) => state.fruit.fruits);
   const { currentPrices, loading, error } = useCurrentPrices();
 
   const [quantities, setQuantities] = useState({});
 
   useEffect(() => {
-     console.log("Dispatching FETCH_FRUITS and FETCH_CURRENT_PRICES from Market");
+    console.log(
+      "Dispatching FETCH_FRUITS and FETCH_CURRENT_PRICES from Market"
+    );
     dispatch({ type: "FETCH_FRUITS" });
     dispatch({ type: "FETCH_CURRENT_PRICES" });
   }, [dispatch]);
@@ -43,51 +53,61 @@ const Market = () => {
   //   });
   // };
 
-  if (error) return <Typography color="error">Error loading prices: {error}</Typography>;
+  if (error)
+    return <Typography color="error">Error loading prices: {error}</Typography>;
   const hasFruits =
     fruits && typeof fruits === "object" && Object.keys(fruits).length > 0;
-    
-    // const handleQuantityChange = (fruitId, value) => {
-    //   setQuantities(prevQuantities => ({
-    //     ...prevQuantities,
-    //     [fruitId]: value
-    //   }));
-    // };
 
-    console.log("Fruits:", fruits);
+  // const handleQuantityChange = (fruitId, value) => {
+  //   setQuantities(prevQuantities => ({
+  //     ...prevQuantities,
+  //     [fruitId]: value
+  //   }));
+  // };
+
+  console.log("Fruits:", fruits);
   console.log("Current Prices:", currentPrices);
-  
+
   return (
     <Container>
-       <Typography variant="h2" gutterBottom>
-      Welcome, {user.username}!
+      <Typography variant="h2" gutterBottom>
+        Welcome, {user.username}!
       </Typography>
 
       {/* <p>Your ID is: {user.id}</p> */}
       {/* <Wallet /> */}
       <Typography variant="h3" gutterBottom>
-      Fruit Market
+        Fruit Market
       </Typography>
 
       {/* <FruitsList />  */}
-      
+
       {hasFruits ? (
         <Grid container spacing={4}>
           {Object.entries(fruits).map(([key, fruit]) => {
-            if (fruit && fruit.current_price != null) { // Check if fruit and current_price are not null
-              const currentPrice = currentPrices[key] != null ? currentPrices[key] : fruit.current_price;
+            if (fruit && fruit.current_price != null) {
+              // Check if fruit and current_price are not null
+              const currentPrice =
+                currentPrices[fruit.id] != null
+                  ? currentPrices[fruit.id]
+                  : fruit.current_price;
               if (!isNaN(parseFloat(currentPrice))) {
                 return (
-                  <Grid item key={key} xs={12} sm={6} md={4}>
+                  <Grid item key={fruit.id} xs={12} sm={6} md={4}>
                     <Card>
                       <CardContent>
                         <Typography variant="h5" component="div">
-                          {capitalizeFirstLetter(replaceUnderscoreWithSpace(fruit.name || ""))}
+                          {capitalizeFirstLetter(
+                            replaceUnderscoreWithSpace(fruit.name || "")
+                          )}
                         </Typography>
                         <Typography variant="body2" className="black-text">
                           Price: ${parseFloat(currentPrice).toFixed(2)}
                         </Typography>
-                      {/* <TextField
+                        <Typography variant="body2" color="textSecondary">
+                          ID: {fruit.id}
+                        </Typography>
+                        {/* <TextField
                         size="small"
                         label="Quantity"
                         type="number"
@@ -104,9 +124,12 @@ const Market = () => {
                           style: { color: "black" },
                         }}
                       /> */}
-                     </CardContent>
+                      </CardContent>
                       <CardActions>
-                        <BuyButton fruit={fruit} quantity={quantities[key] || 1} />
+                        <BuyButton
+                          fruit={fruit}
+                          quantity={quantities[fruit.id] || 1}
+                        />
                       </CardActions>
                     </Card>
                   </Grid>
@@ -122,12 +145,15 @@ const Market = () => {
       <UserInventoryPage currentUser={user} />
       <LogOutButton className="btn" />
     </Container>
-      );
-    };
-    
-      {/* <Inventory inventory={inventory}  */}
+  );
+};
 
-      {/* <ToggleButton
+{
+  /* <Inventory inventory={inventory}  */
+}
+
+{
+  /* <ToggleButton
         isToggled={showAllUsersInventory}
         onToggle={() => setShowAllUsersInventory(!showAllUsersInventory)}
       />
@@ -141,9 +167,11 @@ const Market = () => {
       // (
       //   // <UserInventory inventory={userInventory} />
       // )
-      } */}
+      } */
+}
 
-      {/* <AverageTotal />  */}
-    
+{
+  /* <AverageTotal />  */
+}
 
 export default Market;
